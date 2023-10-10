e7eb8db4a182100ccd07430c7ccc5659a575e61c
saprykins <60004824+saprykins@users.noreply.github.com>
2023-10-10 13:37:03 +0200

    Update ado_pod_extractor.sh

--- ado_pod_extractor.sh
+++ ado_pod_extractor.sh
@@ -7,6 +7,6 @@ ACCOUNT_KEY="1**"
 rm -f *.csv
 rm -f *.xlsx
 
-python 3_main.py
+python main.py
 
 az storage file upload --account-name azuredevopsblobstorage --account-key $ACCOUNT_KEY --share-name azuredevops-fs --source output2.xlsx
